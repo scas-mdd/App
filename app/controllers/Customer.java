e7a984866c6129f247062fb4d8e1c683708c127a
Stefan <scas@itu.dk>
2013-06-02 18:34:19 +0200

    Updated, include reset password with e-mail sent

--- app/controllers/Customer.java
+++ app/controllers/Customer.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.List;
 
 import javax.swing.text.html.FormSubmitEvent;
 
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.apache.log4j.Logger;
 
 
@@ -11,6 +15,7 @@ import play.data.validation.Required;
 import play.data.validation.Valid;
 import play.data.validation.Validation.ValidationResult;
 import play.db.jpa.JPA;
+import play.libs.Mail;
 import util.encryption.BCrypt;
 
 public class Customer extends Application{
@@ -65,23 +70,54 @@ public class Customer extends Application{
 	public static void loginPage(){
 		render();
 	}
+	
+	public static void resetPassword(){
+		render();
+	}
 	public static void login(@Required String email, @Required String password){
-		
+		validation.required(email);
+		if(validation.hasErrors()){
+			render("@loginPage", email);
+		}
 		models.Customer customer = models.Customer.findByEmail(email);
 		if(customer != null){
 			if(BCrypt.checkpw(password, customer.password)){
 				session.put("customer", customer);
 				play.Logger.info("Customer with id "+customer.id + " has logged in.");
+				flash.success("Welcome "+customer.firstName);
+				index();
 			}
 			else{
 				validation.required(password);
 				validation.equals(password, BCrypt.checkpw(password, customer.password)).message("Password not valid");
 				if(validation.hasErrors()){
-					params.current();
 					render("@loginPage", password);
 				}
 			}
 		}
+	}
+	
+	public static void lostPassword(@Required String email) throws EmailException{
+		validation.required(email);
+		if(validation.hasErrors()){
+			render("@resetPassword", email);
+		}
+		SecureRandom random = new SecureRandom();
+		String newPass = new BigInteger(32,random).toString(32);
+		models.Customer customer = models.Customer.findByEmail(email);
+		SimpleEmail e_mail = new SimpleEmail();
+		e_mail.setFrom("handler@drinkstation.com");
+		e_mail.addTo(email);
+		e_mail.setSubject("Password change request");
+		e_mail.setMsg("Dear "+customer.firstName + "\n\n  You or someone else requested to change the password on your account at DrinkStation."
+				+"\n  The new password is "+newPass+".\n " +
+				"\nPlease change your password as soon as possible. "+" \nBest regards, \n  Team DrinkStation");
+		Mail.send(e_mail);
+		customer.password = BCrypt.hashpw(newPass, BCrypt.gensalt());
+		customer.save();
+		play.Logger.info("Password reseted" );
+		
+		flash.success("Password reseted. You will receive an e-mail with your password shortly.");
 	}
 	public static void logout(){
 		session.clear();
